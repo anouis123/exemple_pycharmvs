eea6d0b21806f3f54c5ff3d3e1449bd78821428e
anouis123 <56122929+anouis123@users.noreply.github.com>
2021-07-21 22:32:41 +0100

    now we want to know how to commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print('this our first essaye of git')
+    print('this our 2dn changement')
+    print('how are you')
+    print("the last change")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
